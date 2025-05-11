d0e31af165964665f7f8728b0c855895d9dd3bd9
cemilyener <cemilyener@yandex.com>
2025-05-11 08:30:56 +0300

    Test klasörü oluşturuldu

---
+++ src/utils/chess/PgnImporter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PgnImporter = () => {
+  return (
+    <div>
+      <h3>PGN Importer</h3>
+      <p>PgnImporter bileşeni başarıyla yüklendi.</p>
+    </div>
+  );
+};
+
+export default PgnImporter;
